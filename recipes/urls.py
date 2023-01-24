1d390bd3323fa12a16378d70cee3d9e22bf0c260
Bruno <brunosandrade21@gmail.com>
2023-01-23 22:18:22 -0300

    add template inheritance

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
 from django.urls import path
-from recipes.views import home
+from . import views
 
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', home),
+    path('', views.home, name='home'),
+    path('recipes/<int:id>/', views.recipe, name='recipe'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,39 +1,16 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
-
-recipe_list = [
-    'Ukrainian Pierogi',
-    'Single-Serving Chocolate Chip Cookie', 
-    'Lemon Pepper Chicken Wings', 
-    'Golden Chicken Vegetable Soup With Chickpeas', 
-    'Chocolate Bundt Cake', 
-    'Crispy Spiced Roasted Chickpeas', 
-    'Cincinnati Chili', 
-    'Spicy Sesame Chile Oil Noodles', 
-    'Spaghetti Carbonara', 
-    'Mango Dessert Cup',
-    ]
-
-names = [
-    'Lamar Ortega',
-    'Arthur Odonnell',
-    'Abram Moon',
-    'Jalen Decker',
-    'Emmalee Salinas',
-    'Trey Ramos',
-    'Jadyn Andrews',
-    'Clara Cross',
-    'Johnny Newton',
-    'London Merritt',
-    ]
-
+from utils.recipes.factory import make_recipe
 
 def home(request):
     context = {
-        'names': names,
-        'n': range(10),
-        'recipe_list': recipe_list,
-        'info': zip(names, recipe_list),
+        'recipes': [make_recipe() for _ in range(10)],
     }
     return render(request, 'recipes/pages/home.html', context=context)
+
+
+def recipe(request, id):
+    context = {
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    }
+    return render(request, 'recipes/pages/recipe-view.html', context=context)
