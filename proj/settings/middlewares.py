57d37d69a0f96382f085d257ae2816b0b5efe690
Bruno <brunosandrade21@gmail.com>
2023-03-24 20:10:51 -0300

    add whitenoise middleware

--- proj/settings/middlewares.py
+++ proj/settings/middlewares.py
@@ -1,5 +1,6 @@
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
